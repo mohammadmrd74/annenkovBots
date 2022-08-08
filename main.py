cf19c5c39e3de5aba31c81244040665893920512
Mohammad <mohammadmrd74@gmail.com>
2022-08-08 09:08:55 +0430

    Af

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor(dictionary=True)
 
 if (TYPE == "update"):
-    mycursor.execute("select productId, link, (select brandName from brands where id = brandId) as brand from products where active=0")
+    mycursor.execute("select productId, link, (select brandName from brands where id = brandId) as brand from products")
 else:
     mycursor.execute("select * from links where inserted = 0")
 
